eb08f2cc5ab341747fd98bb5bd7582d0cb9d3e3c
EdenDrori <139678405+EdenDrori@users.noreply.github.com>
2024-01-21 23:01:13 +0200

    Delete srcServer directory

--- srcServer/middleware/validation/index.ts
+++
@@ -1,8 +0,0 @@
-import { joiItemSchema } from "../../joi/item.joi";
-import { joiLoginSchema } from "../../joi/login.joi";
-import { joiUserSchema } from "../../joi/user.joi";
-import { validateSchema } from "./validate-schema";
-
-export const validateUserRegistration = validateSchema(joiUserSchema);
-export const validateUserLogin = validateSchema(joiLoginSchema);
-export const validateNewItem = validateSchema(joiItemSchema)
